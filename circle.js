2bcfd977c782e7a94ae7e21680480075aaab27be
ManyataJain <65446562+ManyataJain@users.noreply.github.com>
2020-08-24 15:12:48 +0530

    Add files via upload

---
+++ circle.js
@@ -0,0 +1,19 @@
+class Circle {
+    constructor(x,y,radius) {
+        
+      this.body = Bodies.circle(x,y,radius);
+      this.radius=radius;
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      var angle=this.body.angle;
+      push();
+      translate(this.body.position.x,this.body.position.y);
+      rotate(angle);
+      fill("green");
+      ellipseMode(RADIUS);
+      ellipse(0,0,this.radius);
+      pop();
+    }
+  }
